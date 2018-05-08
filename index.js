91445c849c34fa106f7103c6f9bf596512b99665
Tiffany K <tiffanykyi@gmail.com>
2018-05-08 16:50:08 -0700

    fix(1007): Add method signature to get coverage links (#6)

--- index.js
+++ index.js
@@ -4,11 +4,39 @@
 class CoverageBase {
     /** Constructor for Coverage plugin
      * @method Constructor
-     * @param {Object}  config  Configuration
-     * @return {CoverageBase}
+     * @param   {Object}    config  Configuration
+     * @return  {CoverageBase}
      */
     constructor(config) {
         this.config = config;
+    }
+
+    /**
+     * Return an access token that build can use to talk to coverage server
+     * @method getAccessToken
+     * @param   {Object}  buildCredentials    Infomation stored in a build JWT
+     * @return  {Promise}                     An access token that build can use to talk to coverage server
+     */
+    getAccessToken(buildCredentials) {
+        return this._getAccessToken(buildCredentials);
+    }
+
+    _getAccessToken() {
+        return Promise.reject('Not implemented');
+    }
+
+    /**
+     * Return coverage links
+     * @method getLinks
+     * @param   {Object}  jobId    Coverage job ID
+     * @return  {Promise}          An object with links to the coverage
+     */
+    getLinks(jobId) {
+        return this._getLinks(jobId);
+    }
+
+    _getLinks() {
+        return Promise.reject('Not implemented');
     }
 
     /**
@@ -21,20 +49,6 @@ class CoverageBase {
     }
 
     _getUploadCoverageCmd() {
-        return Promise.reject('Not implemented');
-    }
-
-    /**
-     * Return an access token that build can use to talk to coverage server
-     * @method getAccessToken
-     * @param {Object}  buildCredentials    Infomation stored in a build JWT
-     * @return {Promise}                    An access token that build can use to talk to coverage server
-     */
-    getAccessToken(buildCredentials) {
-        return this._getAccessToken(buildCredentials);
-    }
-
-    _getAccessToken() {
         return Promise.reject('Not implemented');
     }
 }
